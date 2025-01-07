66e6cbe8aab73d5f3eb4811b7f57e36281e88046
jose del valle <josedelvalle012@gmail.com>
2025-01-07 17:21:33 -0300

    Add: Variables Local Storage y estados Account - Sign Out

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,8 +2,35 @@ import { createContext, useState, useEffect } from "react";
 
 export const ShoppingCartContext = createContext()
 
+export const initializeLocalStorage = () => {
+    const accountInLocalStorage = localStorage.getItem('account')
+    const signOutInLocalStorage = localStorage.getItem('sign-out')
+    let parsedAccount
+    let parsedSignOut
+
+    if (!accountInLocalStorage) {
+        localStorage.setItem('account', JSON.stringify({}))
+        parsedAccount = {}
+    } else {
+        parsedAccount = JSON.parse(accountInLocalStorage)
+    }
+
+    if (!signOutInLocalStorage) {
+        localStorage.setItem('sing-out', JSON.stringify(false))
+        parsedSignOut = false
+    } else {
+        parsedSignOut = JSON.parse(signOutInLocalStorage)
+    }
+}
+
 export const ShoppingCartProvider = ({ children }) => {
-    // Shopping Cart - Increment Quantity
+// My account
+    const [account, setAccount] = useState({})
+
+// Sign Out
+    const [signOut, setSignOut] = useState(false)
+
+// Shopping Cart - Increment Quantity
     const [count, setCount] = useState(0);
 
 // Product Detail - Open/Close
@@ -97,7 +124,11 @@ export const ShoppingCartProvider = ({ children }) => {
             filteredItems,
             setFilteredItems,
             searchByCategory,
-            setSearchByCategory
+            setSearchByCategory,
+            account,
+            setAccount,
+            signOut,
+            setSignOut
         }}>
             {children}
         </ShoppingCartContext.Provider>
